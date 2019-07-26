c7f27e41a6edd58e03b7f116e607ed0c4eadb382
Илия Митвичин <ilia@kodar.net>
2019-07-26 16:22:15 +0300

    component introduction completed

---
+++ ComponentIntruduction/advanced_comunication/src/components/New.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="++counter">Increment</button>
+    <p>{{counter}}</p>
+  </div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      counter: 0
+    };
+  },
+  deactivated(){
+      console.log("Deactivated!");
+  },
+  activated(){
+      console.log("Activated!")
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ ComponentIntruduction/advanced_comunication/src/components/Quote.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <div class="title">
+        <slot name="title"></slot>
+        <slot name="Test">Some string</slot>
+      </div>
+      <div>
+    <slot name="quote"></slot>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+div {
+  margin: 30px auto;
+  text-align: center;
+  padding: 30px;
+  box-shadow: 1px 1px 2px black;
+  border: 1px solid #ccc;
+}
+
+div.title{
+    font-style: italic;
+}
+</style>
+
+
