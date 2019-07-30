0f162e59b22d7bdcc4f00bbf6eaf80689b337adf
Илия Митвичин <ilia@kodar.net>
2019-07-30 17:59:54 +0300

    completed filters and mixins

---
+++ FiltersAndMixins/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <p>{{text | toUppercase | toLowercase}}</p>
+        <hr />
+        <button @click="fruits.push('berries')">Add</button>
+        <input type="text" v-model="filterValue" />
+        <ul>
+          <li v-for="(fruit,i) in filteredFruits" :key="i">{{fruit}}</li>
+        </ul>
+        <hr />
+        <app-list></app-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import List from "./List.vue";
+import { FruitMixin } from './FruitMixin';
+
+export default {
+  data() {
+    return {
+      text: "Some test !",
+    
+    };
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
+  },
+  components: {
+    appList: List
+  },
+  mixins:[FruitMixin]
+};
+</script>
+
+<style>
+</style>
