386d14ac73b790e8def08722f9ce45a73a7605fa
Илия Митвичин <ilia@kodar.net>
2019-08-08 12:23:49 +0300

    finished cli section

---
+++ CLI3/vue-cli-3/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from "../components/HelloWorld";
+
+export default {
+  components: {
+    HelloWorld
+  }
+};
+</script>
+<style lang="scss" scoped>
+footer{
+  small{
+    color: red;
+  }
+}
+</style>
+>
+footer {
+  small {
+  }
+}
+</stylescoped>
